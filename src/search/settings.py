e9bfd731418dcddfe13d6f4603f0e75107f0134b
Gleb Novikov <glebnovikov98@gmail.com>
2021-01-25 12:43:24 +0300

    restructured services

--- src/search/settings.py
+++ src/search/settings.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.sep.join(os.path.normpath(__file__).split(os.sep)[:-2])
+BASE_DIR = os.sep.join(os.path.normpath(__file__).split(os.sep)[:-3])
 BASE_DATA_DIR = os.path.join(BASE_DIR, 'data')
 assert os.path.exists(BASE_DATA_DIR)
 
@@ -11,5 +11,4 @@ def datafile(file):
 
 USER_DATA_FILE = datafile('users.csv')
 GEO_DATA_FILE = datafile('geo.csv')
-REQUESTS_DATA_FILE = ''
 SEARCH_DOCUMENTS_DATA_FILES = [datafile(f'news_generated.{i}.csv') for i in range(1, 4)]
